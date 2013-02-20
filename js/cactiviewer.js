3c9c34d994973a638e0dcc22f89c366b647ad7e5
Yu Gotou <yu.gotou@gmail.com>
2013-02-20 09:58:27 +0900

    とりあえずコミット。

---
+++ js/cactiviewer.js
@@ -0,0 +1,136 @@
+;
+var inhtml = "";
+var spancnt = 0;
+var Year,month,day,hour,minute,second;
+var unixbefor,unixafter;
+$(function() {
+	$('#befordate').datetimepicker({pickTime: false});
+	$('#afterdate').datetimepicker({pickTime: false});
+	$('#befortime').datetimepicker({pickDate: false});
+	$('#aftertime').datetimepicker({pickDate: false});
+	$("#h1title").text(settingList.SiteName);
+	$("#burl").attr("href",settingList.baseurl);
+	var idx = 1;
+	for(var i in itemList){
+		$("#navul").append("<li><a id=\"listidx"+idx+"\" class=\"navli\" href=\"#\">"+itemList[i].titlelabel+"</li>");
+		idx += 1;
+	}
+	dateinit();
+	labelload("listidx1");
+	$(".navli").click(function(e) {
+		labelload($(this).attr("id"));
+	});
+	$("#reloadcheck").click(function(){
+		if ($('#reloadcheck').attr("checked") == "checked"){
+			$("#timeform").css('display', 'none');
+			imgintarvalload();
+			timerID = setInterval(imgintarvalload,60000);
+		} else {
+			$("#timeform").css('display', '');
+			clearInterval(timerID);
+		};
+	});
+	$("#timepickerbtn").click(function(){
+		var befordatetime = $('.befordate').val() + " " + $('.befortime').val();
+		var afterdatetime = $('.afterdate').val() + " " + $('.aftertime').val();
+		befordatetime = new Date(befordatetime);
+		afterdatetime = new Date(afterdatetime);
+		unixbefor = Math.round(befordatetime.getTime() / 1000);
+		unixafter = Math.round(afterdatetime.getTime() / 1000);
+		imgload();
+	});
+});
+//labelclick
+function labelload(selectlabel){
+	var idx = 1;
+	$("#loadcontents").empty();
+	$("#navul").children().attr("class","");
+	$("#"+selectlabel).parent("li").attr("class","active");
+	for(var i in itemList){
+		if(selectlabel == "listidx" + idx){
+			inhtml = "<div class=\"row-fluid\">";
+			spancnt = 0;
+			for(var j in itemList[i].item){
+				imagecreate(itemList[i].item[j].id,itemList[i].item[j].title);
+			}
+			inhtml += "</div>";
+			$("#loadcontents").append(inhtml);
+		}
+		idx += 1;
+	}
+	$(".span6").mouseover(function(){
+		$(this).css("backgroundColor", "AliceBlue");
+	});
+	$(".span6").mouseout(function(){
+		$(this).css("backgroundColor", "white");
+	});
+};
+function imgintarvalload(){
+	var date  = new Date();
+	var arr = datecalc(date,0,0,0,-1,0,0);
+    $(".befordate").val(arr[0]);
+    $(".befortime").val(arr[1]);
+    unixbefor = Math.round(arr[2].getTime() / 1000);
+	var arr = datecalc(date,0,0,0,0,0,0);
+    $(".afterdate").val(arr[0]);
+    $(".aftertime").val(arr[1]);
+    unixafter = Math.round(arr[2].getTime() / 1000);
+	console.log(unixbefor+" "+unixafter);
+
+	imgload();
+}
+function imgload() {
+	$('img').each(function(i){
+		$(this).fadeOut("fast");
+		$(this).attr('src',settingList.baseurl+settingList.beturl+$(this).attr("title")+settingList.starttime+unixbefor+settingList.endtime+unixafter);
+		$(this).fadeIn("fast");
+	});
+}
+//imagecreate
+function imagecreate(urlid,titlelabel){
+	if (typeof urlid !== "undefined") {
+		if(urlid == "hr"){
+			inhtml += "</div><hr><div class=\"row-fluid\">";
+			spancnt = 0;
+		} else {
+			if(spancnt == 2){
+				inhtml += "</div><div class=\"row-fluid\"><div class=\"span6\"><h5>"+titlelabel+"</h5>";
+				spancnt = 1;
+			} else {
+				inhtml += "<div class=\"span6\"><h5>"+titlelabel+"</h5>";
+				spancnt += 1;
+			}
+			inhtml += "<a href=\""+settingList.baseurl+settingList.imgurl+urlid+settingList.imgurl2+settingList.starttime+unixbefor+settingList.endtime+unixafter+"\" target=\"_blank\">";
+			inhtml += "<img class=\"cactiimg\" title=\""+urlid+"\" src=\""+settingList.baseurl+settingList.beturl+urlid+settingList.starttime+unixbefor+settingList.endtime+unixafter+urlid+"\"></a></div>";
+		}
+	}
+};
+
+function dateinit(){
+	var date  = new Date();
+	var arr = datecalc(date,0,0,-1,0,0,0);
+    $(".befordate").val(arr[0]);
+    $(".befortime").val(arr[1]);
+    unixbefor = Math.round(arr[2].getTime() / 1000);
+	var arr = datecalc(date,0,0,0,0,0,0);
+    $(".afterdate").val(arr[0]);
+    $(".aftertime").val(arr[1]);
+    unixafter = Math.round(arr[2].getTime() / 1000);
+};
+
+function datecalc(datetime,y,M,d,h,m,s){
+	Year = datetime.getFullYear() + y;
+	month = datetime.getMonth()+1 + M;
+	day = datetime.getDate() + d;
+	hour   = datetime.getHours() + h;
+	minute = datetime.getMinutes() + m;
+	second = datetime.getSeconds() + s;
+	if (month < 10) {month = "0" + month;}
+	if (day < 10) {day = "0" + day;}
+	if (hour < 10) {hour = "0" + hour;}
+	if (minute < 10) {minute = "0" + minute;}
+	if (second < 10) {second = "0" + second;}
+	var time = Year+"-"+month+"-"+day+" "+hour+":"+minute+":"+second;
+	time = new Date(time);
+	return [Year+"-"+month+"-"+day,hour+":"+minute+":"+second,time];
+};
---
+++ setting.js
@@ -0,0 +1,11 @@
+var settingList =
+{
+	"baseurl":"http://cacti/cacti/",
+	"beturl":"graph_image.php?action=zoom&local_graph_id=",
+	"starttime":"&graph_start=",
+	"endtime":"&graph_end=",
+	"imgurl":"graph.php?action=zoom&local_graph_id=",
+	"imgurl2":"&rra_id=0&view_type=tree",
+	"SiteName":"Cacti Viewer"
+};
+
