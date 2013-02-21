c2abbf544f4dbee2605968ef23a7cd2558c19fab
Yu Gotou <yu.gotou@gmail.com>
2013-02-21 10:54:28 +0900

    ちょいとテスト

--- list.js
+++ list.js
@@ -6,9 +6,25 @@ var itemList =
 		{"id": "453","title": "test2"},
 		{"id": "452","title": "test1"},
 		{"id": "453","title": "test2"},
+		{"id": "453","title": "test3"},
 		{"id": "453","title": "test3"}
 	]
 },
+{
+	"titlelabel":"bbbb",
+	"item": [
+		{"id": "452","title": "1111"},
+		{"id": "453","title": "3333"},
+		{"id": "hr","title": ""},
+		{"id": "462","title": "2222"},
+		{"id": "hr","title": ""},
+		{"id": "542","title": "5555"},
+		{"id": "675","title": "1111"},
+		{"id": "595","title": "1111"},
+		{"id": "617","title": "1111"},
+		{"id": "617","title": "1111"}
+	]
+	},
 {
 	"titlelabel":"bbbb",
 	"item": [
